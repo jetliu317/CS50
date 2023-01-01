9c47715212e84fce54cffc5bbe28e611e7ee6ba5
JetLiu <s717088@gmail.com>
2023-01-01 11:06:34 +0800

    +format

---
+++ Lecture 7/format.py
@@ -0,0 +1,8 @@
+import re
+
+name = input('What\'s your name?' ).strip()
+if matches = re.search(r'^(.+), *(.+)$', name)
+    # last ,first = matches.groups()
+    # name = f{first} {last}
+    name = matches.group(2) + ' ' + matches.group(1)
+print(f'Hello, {name}')
