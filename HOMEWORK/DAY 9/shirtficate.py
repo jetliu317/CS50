30609196d80155a7af9224ba9f0db10347fba9e0
JetLiu <s717088@gmail.com>
2023-01-06 16:12:02 +0800

    finish L8

---
+++ HOMEWORK/DAY 9/shirtficate.py
@@ -0,0 +1,22 @@
+from fpdf import FPDF
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+def main():
+    title()
+    add_name()
+    pdf.output('shirtificate.pdf')
+    
+def title():
+    pdf.add_page()
+    pdf.set_font("helvetica", "B", 50)
+    pdf.cell(0, 60, "CS50 Shirtificate", new_x="LMARGIN", new_y="NEXT", align='C')
+
+def add_name():
+    pdf.set_font("helvetica", "B", 30)
+    pdf.set_text_color(255,255,255)
+    get_name = input('NAME: ')
+    pdf.image('shirtificate.png', w=pdf.epw)
+    pdf.text(x=47.5, y=140, txt = f'{get_name} TOOK CS50')
+
+if __name__ == '__main__':
+    main()
